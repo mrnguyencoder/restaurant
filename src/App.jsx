0257b8295a007cb4a4cb28da16c27b6dba0c8e36
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-02-13 23:01:18 +0100

    topbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Menu from "./components/Menu"
 import NavBar from "./components/NavBar"
 import Reservation from "./components/Reservation"
 import Testimonial from "./components/Testimonial"
+import Topbar from "./components/Topbar"
 
 
 function App() {
   return (
     <>
+    <Topbar/>
     <NavBar/>
     <Hero/>
     <Reservation/>
