4bd89a7c384dbcb2ee63cf125784e950fc615092
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-02-13 21:42:39 +0100

    Reservation change

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -1,19 +1,10 @@
 import React, { useState } from 'react';
-
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 import TimePicker from 'react-time-picker';
-import '../timepicker.css';
-// import icons
 import { FaUsers, FaCalendar, FaClock } from 'react-icons/fa';
-// import motion
-import { motion } from 'framer-motion';
-// import variants
-import { fadeIn, staggerContainer } from '../variants';
+
 
 const Reservation = () => {
-  // destructure reservation data
-  const { title, subtitle, modelImg, btnText } = reservationData;
   // date state
   const [startDate, setStartDate] = useState(new Date());
   // clock state
@@ -22,41 +13,8 @@ const Reservation = () => {
   return (
     <section className='relative top-96 z-30 pb-20 lg:py-[100px]'>
       <div className='container mx-auto'>
-        {/* text */}
-        <motion.div
-          variants={staggerContainer}
-          initial='hidden'
-          whileInView={'show'}
-          className='text-center'
-        >
-          {/* title */}
-          <motion.h2
-            variants={fadeIn('up', 'tween', 0.2, 1.6)}
-            className='h2 capitalize'
-          >
-            {title}
-          </motion.h2>
-          {/* subtitle */}
-          <motion.p
-            variants={fadeIn('up', 'tween', 0.4, 1.6)}
-            className='mb-8 text-dark'
-          >
-            {subtitle}
-          </motion.p>
-          {/* model */}
-          <motion.div
-            variants={fadeIn('up', 'tween', 0.6, 1.6)}
-            className='flex justify-center mb-8'
-          >
-            <img src={modelImg} alt='' />
-          </motion.div>
-        </motion.div>
+
         {/* form */}
-        <motion.form
-          variants={fadeIn('up', 'tween', 0.7, 1.6)}
-          initial='hidden'
-          whileInView={'show'}
-        >
           <div className='flex flex-col lg:flex-row gap-y-4 items-center justify-between mb-8'>
             {/* datepicker */}
             <div>
@@ -99,7 +57,6 @@ const Reservation = () => {
               {btnText}
             </button>
           </div>
-        </motion.form>
       </div>
     </section>
   );
